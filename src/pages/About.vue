ca641a19c5f08f072a5713d24a58d5a257ac3d6f
Daniel <daniel.greenfeld@britecore.com>
2019-06-29 15:36:05 -0700

    About fix

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -24,6 +24,8 @@
     <div class="content-box">
       <div class="post__content">       
 
+        <p>About Daniel Roy Greenfeld</p>
+
         <p><strong>Daniel Roy Greenfeld</strong> has over {{ new Date().getFullYear() - 1997 }} years of experience in the technology industry and a depth of knowledge in computer programming languages including Python, Django, JavaScript, and Vue.js. He previously worked as a software engineer for NASA where he twice earned an Award for Excellence for his work on numerous large-scale software systems. Daniel also co-authored the <a href="https://twoscoopspress.com">Two Scoops of Django: Best Practices</a> book series and created numerous large open-source projects such as Cookiecutter Django. He is a Fellow of the Python Software Foundation and received the Frank Willison Memorial Award for contributions to Python. At <a href="https://britecore.com">BriteCore</a>, Daniel serves as VP of Implementation Engineering, where he oversees various aspects of engineering.</p>
 
         <p>In his free time, Daniel enjoys spending time with his wife and daughter, contributing to open source, writing fantasy and science fiction, and cycling.</p>
